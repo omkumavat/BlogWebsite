00726d51140fd49efc2a7a01c4b155d40e428b89
Shivaji Gadekar <gadekarshivaji8767@gmail.com>
2025-03-25 13:31:35 +0530

    server

---
+++ Server/Models/User.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String,required:true },
+  profilePic: { type: String },
+  blogs: [{ type: mongoose.Schema.Types.ObjectId, ref: "Blog" }]
+},{timestamps:true});
+
+const User=mongoose.model("User", UserSchema);
+export default User;
